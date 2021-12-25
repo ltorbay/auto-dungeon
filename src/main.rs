275c92d5cef0dbf0a57ea86a6f8f956728a581ed
ltorbay <ltorb@dolby.com>
2021-12-25 19:03:50 +0100

    Add printing full grid at start

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,17 @@ use sdl2::gfx::primitives::DrawRenderer;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels;
 use sdl2::pixels::Color;
-use sdl2::render::{Canvas, WindowCanvas};
+use sdl2::render::Canvas;
 use sdl2::video::Window;
 
-use tiles::Coordinates;
+use tiles::{Coordinates, Grid};
 use tiles::Hexagon;
 
 mod tiles;
 
-const SCREEN_WIDTH: i16 = 800;
-const SCREEN_HEIGHT: i16 = 600;
-const ORIGIN: (i16, i16) = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+// const SCREEN_WIDTH: i16 = 800;
+// const SCREEN_HEIGHT: i16 = 600;
+// const ORIGIN: (i16, i16) = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
 
 fn main() {
     draw().expect("Failed drawing")
@@ -26,53 +26,67 @@ fn main() {
 fn draw() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsys = sdl_context.video()?;
+
+    let (screen_width, screen_height) = video_subsys.display_bounds(0)?.size();
+    let origin = (screen_width / 2, screen_height / 2);
+    
     let window = video_subsys
         .window(
             "Auto dungeon",
-            SCREEN_WIDTH as u32,
-            SCREEN_HEIGHT as u32,
+            screen_width,
+            screen_height,
         )
-        .position_centered()
+        .fullscreen_desktop()
         .opengl()
         .build()
         .map_err(|e| e.to_string())?;
-
+    
     let mut canvas = window.into_canvas()
         .build()
         .map_err(|e| e.to_string())?;
 
+    canvas.set_draw_color(pixels::Color::RGB(0, 0, 0));
+    canvas.clear();
+    
     let base_color = pixels::Color::RGB(255, 255, 0);
 
-    let mut r = 0;
-    let mut q = 0;
+    // let mut r = 0;
+    // let mut q = 0;
 
-    new_hexagon(&mut canvas, base_color, r, q)?;
+    let grid = Grid::new(origin, 2, 50)?;
+    grid.hexagons.iter().for_each(|tuple| {
+        println!("Creating {:?} for {:?}", tuple.1, tuple.0);
+        canvas.polygon(&tuple.1.x, &tuple.1.y, base_color)
+            .expect("Could not create polygon")
+    });
+    canvas.present();
 
     let mut events = sdl_context.event_pump()?;
     'main: loop {
         for event in events.poll_iter() {
             match event {
                 Event::Quit { .. } => break 'main,
+                Event::KeyDown { keycode: Option::Some(Keycode::Escape), .. } => break 'main,
 
-                Event::KeyDown { keycode: Option::Some(Keycode::Up), .. } => {
-                    r -= 1;
-                    new_hexagon(&mut canvas, base_color, r, q)?;
-                }
-
-                Event::KeyDown { keycode: Option::Some(Keycode::Down), .. } => {
-                    r += 1;
-                    new_hexagon(&mut canvas, base_color, r, q)?;
-                }
-
-                Event::KeyDown { keycode: Option::Some(Keycode::Left), .. } => {
-                    q -= 1;
-                    new_hexagon(&mut canvas, base_color, r, q)?;
-                }
-
-                Event::KeyDown { keycode: Option::Some(Keycode::Right), .. } => {
-                    q += 1;
-                    new_hexagon(&mut canvas, base_color, r, q)?;
-                }
+                // Event::KeyDown { keycode: Option::Some(Keycode::Up), .. } => {
+                //     r -= 1;
+                //     new_hexagon(&mut canvas, base_color, r, q)?;
+                // }
+                // 
+                // Event::KeyDown { keycode: Option::Some(Keycode::Down), .. } => {
+                //     r += 1;
+                //     new_hexagon(&mut canvas, base_color, r, q)?;
+                // }
+                // 
+                // Event::KeyDown { keycode: Option::Some(Keycode::Left), .. } => {
+                //     q -= 1;
+                //     new_hexagon(&mut canvas, base_color, r, q)?;
+                // }
+                // 
+                // Event::KeyDown { keycode: Option::Some(Keycode::Right), .. } => {
+                //     q += 1;
+                //     new_hexagon(&mut canvas, base_color, r, q)?;
+                // }
 
                 _ => {}
             }
@@ -83,11 +97,11 @@ fn draw() -> Result<(), String> {
     Ok(())
 }
 
-fn new_hexagon(canvas: &mut Canvas<Window>, base_color: Color, r: i16, q: i16) -> Result<(), String> {
-    let hexagon = Hexagon::new(ORIGIN, Coordinates { r, q }, 50)?;
-    canvas.polygon(&hexagon.x, &hexagon.y, base_color)?;
-    canvas.present();
-    println!("Created {:?}", hexagon);
-    
-    Ok(())
-}
+// fn new_hexagon(canvas: &mut Canvas<Window>, base_color: Color, r: i16, q: i16) -> Result<(), String> {
+//     let hexagon = Hexagon::new(ORIGIN, &Coordinates { r, q }, 50);
+//     canvas.polygon(&hexagon.x, &hexagon.y, base_color)?;
+//     canvas.present();
+//     println!("Created {:?}", hexagon);
+// 
+//     Ok(())
+// }
--- src/tiles.rs
+++ src/tiles.rs
@@ -1,4 +1,4 @@
-use std::num;
+use std::collections::HashMap;
 
 const SMALL_SIDE_LENGTH: f32 = 0.866;
 const X_TEMPLATE: [f32; 6] = [1., 0.5, -0.5, -1., -0.5, 0.5];
@@ -11,22 +11,63 @@ pub struct Hexagon {
 }
 
 impl Hexagon {
-    pub fn new(origin: (i16, i16), coordinates: Coordinates, size: i16) -> Result<Hexagon, &'static str> {
-        let offset = coordinates.as_offset(size);
-        let x = X_TEMPLATE.map(|f| (f * size as f32).round() as i16 + origin.0 + offset.0);
-        let y = Y_TEMPLATE.map(|f| (f * size as f32).round() as i16 + origin.1 + offset.1);
-        Ok(Hexagon { x, y })
+    pub fn new(origin: (u32, u32), coordinates: &Coordinates, pixel_per_hexagon: i16) -> Hexagon {
+        let offset = coordinates.as_offset(pixel_per_hexagon);
+        let x = X_TEMPLATE.map(|f| (f * pixel_per_hexagon as f32).round() as i16 + origin.0 as i16 + offset.0);
+        let y = Y_TEMPLATE.map(|f| (f * pixel_per_hexagon as f32).round() as i16 + origin.1 as i16 + offset.1);
+        Hexagon { x, y }
     }
 }
 
+#[derive(Hash, PartialEq, Eq, Clone, Copy, Debug)]
 pub struct Coordinates {
     pub q: i16,
     pub r: i16,
 }
 
 impl Coordinates {
-    pub fn as_offset(&self, size: i16) -> (i16, i16) {
-        ((size as f32 * 1.5 * self.q as f32).round() as i16,
-         (size as f32 * (self.q as f32 * SMALL_SIDE_LENGTH + self.r as f32 * 2. * SMALL_SIDE_LENGTH)).round() as i16)
+    pub fn as_offset(&self, pixel_per_hexagon: i16) -> (i16, i16) {
+        let offset = ((pixel_per_hexagon as f32 * 1.5 * self.q as f32).round() as i16,
+                      // (pixel_per_hexagon as f32 * (self.q as f32 * SMALL_SIDE_LENGTH + self.r as f32 * 2. * SMALL_SIDE_LENGTH)).round() as i16);
+                      ((pixel_per_hexagon as f32 * SMALL_SIDE_LENGTH).round() * (self.q as f32 + self.r as f32 * 2.)) as i16);
+        println!("offset {:?}", offset);
+        offset
+    }
+
+    pub fn distance_to(&self, to: &Coordinates) -> i16 {
+        let vec_distance = [self.q - to.q, self.r - to.r, self.s() - to.s()];
+        let vec: Vec<i16> = vec_distance.iter().map(|value| value.abs()).collect();
+        let result: i16 = vec.iter().sum();
+        result / 2
+    }
+
+    fn s(&self) -> i16 {
+        -self.q - self.r
+    }
+}
+
+pub struct Grid {
+    pub hexagons: HashMap<Coordinates, Hexagon>,
+    pub q_max: i16,
+    pub r_max: i16,
+}
+
+impl Grid {
+    pub fn new(origin: (u32, u32), radius: i16, pixel_per_hexagon: i16) -> Result<Grid, &'static str> {
+        let center = Coordinates { q: 0, r: 0 };
+        let mut qr_vec = Vec::new();
+        for q in -radius..=radius {
+            for r in -radius..=radius {
+                let target_coordinates = Coordinates { q, r };
+                if center.distance_to(&target_coordinates) <= radius {
+                    qr_vec.push(target_coordinates);
+                }
+            }
+        }
+        let hexagons = qr_vec.iter()
+            .map(|coordinate| (*coordinate, Hexagon::new(origin, coordinate, pixel_per_hexagon)))
+            .collect();
+
+        Ok(Grid { hexagons, q_max: radius, r_max: radius })
     }
 }
